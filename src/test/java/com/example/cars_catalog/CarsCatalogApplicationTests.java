b6f7f7f2dcc81d4afe2e02b27d06d9c8f4e10c4e
Elena Simonova <lenka-simonova@mail.ru>
2021-12-05 01:45:40 +0300

    Created a new project

---
+++ src/test/java/com/example/cars_catalog/CarsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cars_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
