851e12cf84f9c7e7ba5787a5fcd9d493fb8171d3
lenadelrey <lenka-simonova@mail.ru>
2022-01-08 08:26:21 +0300

    fixed flyway migrations, updated build.gradle

--- src/main/resources/db/migration/V002__create_new_table_users.sql
+++ src/main/resources/db/migration/V002__create_new_table_users.sql
@@ -4,7 +4,8 @@ create table users
         constraint users_pkey
             primary key,
     name     varchar(200) not null,
-    email    varchar(254) not null,
+    email    varchar(254) not null UNIQUE,
     password varchar(300) not null,
-    role     varchar(100) not null
+    role_id  bigserial    not null,
+    constraint fk_role_id foreign key (role_id) references roles (role_id)
 );
